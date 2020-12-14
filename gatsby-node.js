b17f72232f374a26c8151e7697d958e616e64943
Julien Poissonnier <julien@gatsbyjs.com>
2020-12-14 15:01:46 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,7 +34,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const arrays = []
   while (true) {
     console.log(process.memoryUsage())
-    arrays.push(new Array(1024 * 1024 * 512).fill(0))
+    arrays.push(new Array(1024 * 1024 * 512).fill(1))
     await new Promise(resolve => setTimeout(resolve, 2 * 1000))
   }
 

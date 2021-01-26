b42a9d2e77c1790d003df2ce38fb0f65c33aaef8
Julien Poissonnier <julien@caffeine.lu>
2021-01-26 16:04:45 +1300

    return body

--- src/api/hello.js
+++ src/api/hello.js
@@ -1,11 +1,16 @@
 export default async (req, res) => {
   const response = await fetch(`https://google.com`)
   console.log({ status: response.status })
+  let body
+  if (response) {
+    body = await response.text()
+  }
 
   const name = req.query.name
   res.status(200).send({
     value: `Hello, ${name}!`,
     status: response.status,
+    body,
     request: {
       path: req.path,
       query: req.query,

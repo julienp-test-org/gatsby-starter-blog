a5a16111c83f27cc6656dbf57b041a802706f2f4
Julien Poissonnier <julien@caffeine.lu>
2021-01-26 16:01:44 +1300

    Use fetch in function

--- src/api/hello.js
+++ src/api/hello.js
@@ -1,7 +1,11 @@
-export default (req, res) => {
+export default async (req, res) => {
+  const response = await fetch(`https://google.com`)
+  console.log({ status: response.status })
+
   const name = req.query.name
   res.status(200).send({
     value: `Hello, ${name}!`,
+    status: response.status,
     request: {
       path: req.path,
       query: req.query,
